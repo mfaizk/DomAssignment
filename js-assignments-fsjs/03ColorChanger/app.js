d29f3c782cd02f8f8fe67c6a683ca64d1d00e271
mfaizk <suneater1413@gmail.com>
2022-11-24 22:30:42 +0530

    clean-up

--- js-assignments-fsjs/03ColorChanger/app.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#button").addEventListener("click", () => {
-  var canvas = document.querySelector("#canvas");
-  canvas.style.cssText = `background-color:rgb(${Math.floor(
-    Math.random() * 255
-  )},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
-});
